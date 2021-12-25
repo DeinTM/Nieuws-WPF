062b48ed429d14ec9f1d36b87e26a2ee9433ec30
DeinTM <r0830956@student.thomasmore.be>
2021-12-25 14:27:39 +0100

    chore: connection database

---
+++ DehouwerDein_a2.1_DM_Project/DAL/DatabaseOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DehouwerDein_a2._1_DM_Project.DAL
+{
+    public static class DatabaseOperations
+    {
+        public static List<Gebruiker> OphalenGebruikers()
+        {
+            using (NieuwsEntities entities = new NieuwsEntities())
+            {
+                var query = entities.Gebruiker;
+                return query.ToList();
+            }
+        }
+    }
+}
